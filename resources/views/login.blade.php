34b6f6dee6ea77bc4b432924991b55420df66a56
Priscila Otaro <priscilaotaro.g26@gmail.com>
2023-07-07 20:25:17 -0300

    se realizó un cambio en la estructura HTML del login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,24 +2,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>QUICKHAUL</title>
+     <title>QuickHaul - Login</title>
         <link rel="stylesheet" href="{{asset('/css/style-login.css')}}">
         <script src="{{asset('/js/login.js')}}"></script>
 
         <header>
-          <div class="box">
-            <img src="/img/fuente_quick_haul.png" width="300px" length="177px"> 
-            <img src="/img/logopweb1.png" width="150px" length="150px">
-          </div>
+         
         </header>
       </head>
-  
+  <br>
+  <br>
       <body>
         <section class="contenido-login">
-        <h2>Ingreso de Usuario</h2>
         
         <form id="form-login" action="/login" method="POST">
+        <div class="box">
+            <img src="/img/fuente_quick_haul.png" width="300px" length="177px"> 
+          </div>
+          <br>
+          
             @csrf
+            <h1>Ingreso de Usuario</h1>
             <input type="text" id="token" placeholder="Ingrese su token" required>
             <input type="password" id="contrasenha" placeholder="Contraseña" required>
             
