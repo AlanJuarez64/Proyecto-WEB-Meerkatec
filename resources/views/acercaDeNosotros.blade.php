eacc1fafc26f64d77cd77f559651f73533927d84
Priscila Otaro <priscilaotaro.g26@gmail.com>
2023-07-08 13:41:08 -0300

    Se aprolijó el código HTML

--- resources/views/acercaDeNosotros.blade.php
+++ resources/views/acercaDeNosotros.blade.php
@@ -1,61 +1,62 @@
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <title>QuickHaul</title>
-    <header>
-        <div class="caja">
-          <a href="/"><img src="img/fuente_quick_haul.png" width="250px" length="250px"></a> 
-          <nav class="inicio">
-            <ul>
-              <li><a href="/">Página Principal</a></li>
-              <li><a href="/acerca-de-nosotros">Acerca de Nosotros</a></li>
-              <li><a href="/contacto">Contacto</a></li>
-              <li class="despliegue">
-                <a href="/servicios">Servicios</a>
-                <div class="despliegue-contenido">
-                  <a href="" class="envios">Envios</a>
-                  <a href="" class="rastreos">Rastreos</a>
-                </div>
-              </li>
-            </ul>
-          </nav>
-          <a href="/login"> <input id="btnlogin" type="button" value="Iniciar Sesión"></a> 
-        </div>
-      </header>   
-      
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+  <title>QuickHaul</title>
+  <header>
+    <div class="caja">
+      <a href="/"><img src="img/fuente_quick_haul.png" width="250px" length="250px"></a>
+      <nav class="inicio">
+        <ul>
+          <li><a href="/">Página Principal</a></li>
+          <li><a href="/acerca-de-nosotros">Acerca de Nosotros</a></li>
+          <li><a href="/contacto">Contacto</a></li>
+          <li class="despliegue">
+            <a href="/servicios">Servicios</a>
+            <div class="despliegue-contenido">
+              <a href="" class="envios">Envios</a>
+              <a href="" class="rastreos">Rastreos</a>
+            </div>
+          </li>
+        </ul>
+      </nav>
+      <a href="/login"> <input id="btnlogin" type="button" value="Iniciar Sesión"></a>
+    </div>
+  </header>
+
 </head>
 
 <body>
-    <br>
-    
- 
-    <h1 class="titulo-principal">Acerca de nuestra empresa.</h1>
-    <h2 class="subtitulo">¿Quiénes somos?</h2>  
-    <section class="mision-vision">
-        <div id="acerca-de"> 
-        
-            <h3>Somos QUICKCARRY</h3>
-            <p id="mision" ><em> <strong>Una empresa cuya misión es transformar la forma en que se mueven las mercancías, 
-                    brindando soluciones de transporte rápido y eficiente que satisfagan las necesidades de nuestros clientes.</strong>
-                  <p>Nos esforzamos por ser reconocidos como un referente en el sector, 
-                    destacándonos por nuestra calidad de servicio y compromiso con la satisfacción del cliente.</p>  
-           </em></p>
-            
-            <p id= "vision" ><em> <strong>En QUICKCARRY, nuestra vision es ofrecer soluciones de transporte rápidas, 
-                seguras y confiables que impulsen el crecimiento y el éxito de nuestros clientes.</p></strong>
+  <br>
 
-             <p> Nos dedicamos a simplificar y agilizar el proceso de envío, brindando una experiencia sin complicaciones y un servicio excepcional en cada entrega.</p>  
 
-               <p>Trabajamos en estrecha colaboración con nuestros clientes, entendiendo sus necesidades únicas y adaptándonos a sus requerimientos específicos. 
-               <p>Nos esforzamos por superar las expectativas, proporcionando soluciones eficientes y asequibles que permitan a nuestros clientes enfocarse en su negocio principal sin preocupaciones logísticas. 
-                En QuickCarry, nuestro compromiso es ser su socio confiable en el transporte, brindando resultados excepcionales y construyendo relaciones duraderas basadas en la confianza y la satisfacción mutua.</p></strong>
-                </em></p></div>
+  <h1 class="titulo-principal">Acerca de nuestra empresa.</h1>
+  <h2 class="subtitulo">¿Quiénes somos?</h2>
+  <section class="mision-vision">
+    <div id="acerca-de">
+
+      <h3>Somos QUICKCARRY</h3>
+      <p id="mision"><em> <strong>Una empresa cuya misión es transformar la forma en que se mueven las mercancías,
+            brindando soluciones de transporte rápido y eficiente que satisfagan las necesidades de nuestros clientes.</strong>
+          <p>Nos esforzamos por ser reconocidos como un referente en el sector,
+            destacándonos por nuestra calidad de servicio y compromiso con la satisfacción del cliente.</p>
+        </em></p>
+
+      <p id="vision"><em> <strong>En QUICKCARRY, nuestra vision es ofrecer soluciones de transporte rápidas,
+            seguras y confiables que impulsen el crecimiento y el éxito de nuestros clientes.</p></strong>
+
+      <p> Nos dedicamos a simplificar y agilizar el proceso de envío, brindando una experiencia sin complicaciones y un servicio excepcional en cada entrega.</p>
+
+      <p>Trabajamos en estrecha colaboración con nuestros clientes, entendiendo sus necesidades únicas y adaptándonos a sus requerimientos específicos.
+      <p>Nos esforzamos por superar las expectativas, proporcionando soluciones eficientes y asequibles que permitan a nuestros clientes enfocarse en su negocio principal sin preocupaciones logísticas.
+        En QuickCarry, nuestro compromiso es ser su socio confiable en el transporte, brindando resultados excepcionales y construyendo relaciones duraderas basadas en la confianza y la satisfacción mutua.</p></strong>
+      </em></p>
     </div>
-</section>
-    <footer>
-        <img src="img/logopweb1.png" width="177" length="250" alt="Logo de QuickHaul">
-        <p class="copyright"> &copy; Copyright QUICKCARRY - 2023</p>
-    </footer>
+    </div>
+  </section>
+  <footer>
+    <img src="img/logopweb1.png" width="177" length="250" alt="Logo de QuickHaul">
+    <p class="copyright"> &copy; Copyright QUICKCARRY - 2023</p>
+  </footer>
 </body>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,37 +1,40 @@
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>QuickHaul - Login</title>
-        <link rel="stylesheet" href="{{asset('/css/style-login.css')}}">
-        <script src="{{asset('/js/login.js')}}"></script>
 
-        <header>
-         
-        </header>
-      </head>
-  <br>
-  <br>
-      <body>
-        <section class="contenido-login">
-        
-        <form id="form-login" action="/login" method="POST">
-        <div class="box">
-            <img src="/img/fuente_quick_haul.png" width="300px" length="177px"> 
-          </div>
-          <br>
-          <br>
-            @csrf
-            
-            <input type="text" id="token" placeholder="Ingrese su usuario" required>
-            <input type="password" id="contrasenha" placeholder="Contraseña" required>
-            
-            <div>
-              <button type="submit">Iniciar sesión</button>
-            </div>
-           </form>
-        </section>
-      
-        
-      </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>QuickHaul - Login</title>
+  <link rel="stylesheet" href="{{asset('/css/style-login.css')}}">
+  <script src="{{asset('/js/login.js')}}"></script>
+
+  <header>
+
+  </header>
+</head>
+<br>
+<br>
+
+<body>
+  <section class="contenido-login">
+
+    <form id="form-login" action="/login" method="POST">
+      <div class="box">
+        <img src="/img/fuente_quick_haul.png" width="300px" length="177px">
+      </div>
+      <br>
+      <br>
+      @csrf
+
+      <input type="text" id="token" placeholder="Ingrese su usuario" required>
+      <input type="password" id="contrasenha" placeholder="Contraseña" required>
+
+      <div>
+        <button type="submit">Iniciar sesión</button>
+      </div>
+    </form>
+  </section>
+
+
+</body>
+
 </html>
