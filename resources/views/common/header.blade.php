ecde4bc8fcfe18b8321d6fe8f28b5a5c95e444aa
Alan Juarez <alanjuarez.n64@gmail.com>
2023-09-13 18:26:05 -0300

    Se agrego un icono de la pagina web para el navegador

--- resources/views/common/header.blade.php
+++ resources/views/common/header.blade.php
@@ -3,7 +3,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="{{asset('css/style.css')}}">
-  <link rel="website icon" href="/img/icon.png">
+  <link rel="website icon" href="/img/iconweb.png">
   <title>QuickHaul</title>
     <header>
         <div class="caja">
