ed2ffc25cb4368acade449afa2e66fe86ee48cfa
Alan Juárez <alanjuarez.n64@gmail.com>
2023-10-19 14:34:32 -0300

    Se ordeno la homepage y se agregaron imagenes y enlaces a acerca de nosotros

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,43 +4,53 @@
   <section class="copywriting">
     <br>
     <div id="intro">
-      <h2>¡Redefine la forma en que te mueves con QuickHaul!</h2>
-      <p>Nuestra innovadora aplicación web de transporte te ofrece una solución rápida,
-        eficiente y confiable para todas tus necesidades de entrega y logística.</p>
-
+      <h2 class="intro">Redefine la forma en que te mueves con</h2>
+      <br>
+      <img class="img-quickhaul"src="/img/fuente_quick_haul.png">
+      <br><br>
       <div class="stanza">
-        <h2>» Con QuickHaul, el poder de un transporte ágil y veloz está en tus manos.</h2>
-      </div>
-
-      <div class="stanza">
+        <div>
         <h3>» Despídete de las largas esperas y los retrasos interminables.</h3>
-        <p>Con QuickHaul, tus envíos son un verdadero viaje express.</p>
-        <p>Nuestra plataforma intuitiva y fácil de usar te permite solicitar y programar tus transportes en cuestión de segundos.</p>
+        <p>Con QuickHaul, tus envíos son un verdadero viaje express.
+          Nuestra plataforma intuitiva y fácil de usar te permite solicitar y
+           programar tus transportes en cuestión de segundos. 
+           <br><br><btn class="btn-sabermas"><a href="/acerca-de-nosotros"><b>Saber mas</b><btn></a></p>
+          </div>
+          <img class="img-right" src="/img/img_home1.jpg">
+      </div>
+      <br><br>
+      <div class="stanza">
+      <img class="img-left" src="/img/img_home2.jpg">
+        <div>
+        <h3>¿Necesitas un envío urgente? No hay problema.</h3>
+        <p> Con nuestra red de transporte confiable y nuestros conductores profesionales,
+        tus mercancías llegarán a su destino en tiempo récord.
+        <br><br><btn class="btn-sabermas"><a href="/acerca-de-nosotros"><b>Saber mas</b><btn></a></p>
+      </div>
       </div>
 
       <div class="stanza">
-        <h3>¿Necesitas un envío urgente? No hay problema. Con nuestra red de transporte confiable y nuestros conductores profesionales,
-          tus mercancías llegarán a su destino en tiempo récord.</h3>
-      </div>
-
-      <div class="stanza">
+        <div>
         <h2>» Con QuickHaul, la eficiencia es nuestro sello distintivo.</h2>
-        <p>Nuestra tecnología avanzada optimiza las rutas y maximiza la capacidad de carga para garantizar entregas rápidas y costos competitivos.
-          Ya sea que necesites enviar un paquete pequeño o gestionar una cadena de suministro completa, QuickHaul te respalda en cada paso del camino.</p>
+        <p>Nuestra tecnología avanzada optimiza las rutas y maximiza la 
+          capacidad de carga para garantizar entregas rápidas y costos competitivos.
+          Ya sea que necesites enviar un paquete pequeño o gestionar una cadena de 
+          suministro completa, QuickHaul te respalda en cada paso del camino.
+          <br><br><btn class="btn-sabermas"><a href="/acerca-de-nosotros"><b>Saber mas</b><btn></a></p>
+      </div>
+      <img class="img-right" src="/img/img_home3.jpg">
+
       </div>
 
       <div class="stanza">
+      <img class="img-left" src="/img/img_home4.jpg">
+      <div>
         <h2>» Olvídate de los dolores de cabeza logísticos.</h2>
-        <h3>Confía en QuickHaul para superar tus expectativas de transporte.</h3>
-        <p>Nuestro compromiso con la calidad y el servicio al cliente nos distingue.</p>
-        <p>Nuestro equipo dedicado está listo para brindarte asistencia las 24 horas del día, los 7 días de la semana,
-          para que tengas total tranquilidad mientras tus envíos están en movimiento.</p>
+        <p>Nuestro equipo dedicado está listo para brindarte asistencia las 24 horas del
+        día, los 7 días de la semana, para que tengas total tranquilidad mientras tus 
+        envíos están en movimiento.
+        <br><br><btn class="btn-sabermas"><a href="/acerca-de-nosotros"><b>Saber mas</b><btn></a></p>
       </div>
-
-      <div class="stanza">
-        <h1>Prepárate para un nuevo nivel de transporte eficiente.</h1>
-        <h4>Únete a la revolución QuickHaul y descubre cómo el poder de la velocidad y la confiabilidad pueden transformar tu negocio. Descarga nuestra aplicación web hoy mismo y experimenta la magia de QuickHaul.
-          ¡Tu carga, nuestro compromiso!</h4>
       </div>
     </div>
   </section>
