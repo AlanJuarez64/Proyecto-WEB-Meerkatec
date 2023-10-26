72603f4a6821630065ddfb53d34504183b199e2b
Alan Juárez <alanjuarez.n64@gmail.com>
2023-10-26 13:52:50 -0300

    Se arreglo la vista de servicios y se agregaron enlaces a las vistas de estos

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -1,40 +1,37 @@
 @include("common/header")
 <body>
-  <br>
-  <section class="soluciones" id="intro">
-    <h1> <strong>» Soluciones de Negocio</strong></h1>
-    QuickHaul te ofrece una variedad de servicios a medida para empresas. Independientemente del tipo de negocio que usted desempeñe, podemos ayudarle a ser más eficiente y exitoso.
-    Por servicios a medida para empresas envíenos un e-mail a quickcarry@contacto.com.uy detallando sus necesidades.
-  </section>
-
   <section class="servicios">
     <div id="intro">
-      <h2>» <strong>Servicios logísticos</strong></h2>
-
-      <h3>Distribución</h3>
-      <p>Amplié su mercado con QuickHaul. Llegue a más clientes con nuestra eficiente red de distribución a todo el país.</p>
-
-      <h3>Consolidación</h3>
-      <p>Nuestra asociación con CRECOM nos permite consolidar sus pedidos a múltiples proveedores, reduciendo el número de envíos y optimizando los costos de transporte.</p>
-
-      <h3>Almacenamiento</h3>
-      <p>CRECOM cuenta con varios depósitos para apoyar la logística de nuestros clientes. Estas instalaciones integran y se adaptan a su cadena de suministros.</p>
-
-      <h3>Cadena de suministros</h3>
-      <p>Deje que QuickHaul centralice sus proveedores y mejore su eficiencia con nuestros servicios de administración de cadena de suministros.</p>
-
-      <h3>Etiquetado</h3>
-      <p>Ahorre tiempo y reduzca sus costos. Servicio de impresión y aplicación de etiquetas directamente al producto o al empaquetado,
-        ya sean datos del destinatario, códigos de barra, precios, o cualquier otro tipo de información.</p>
-
+      <div class="stanza">
+      <div>
       <h3>Rastreo</h3>
-      <p>Información en tiempo real sobre el estado de todas sus envíos a través de nuestra aplicación web.</p>
-
+      <p>Información en tiempo real sobre el estado de todas sus envíos a través de nuestra aplicación web.
+      <br><br><btn class="btn-servicios"><a href="/rastreos"><b>Rastreos</b><btn></a></p>
+      </div>
+      <img class="img-right" src="/img/img_localizar.jpg">
+      </div>
+      <br><br>
+      <div class="stanza">
+      <div>
       <h3>Confirmación de Entrega</h3>
-      <p>Si usted lo desea podemos confirmar al instante de la entrega vía e-mail o por teléfono.</p>
-      </p>
+      <p>Si usted lo desea podemos confirmar al instante de la entrega vía e-mail o por teléfono.
+      <br><br><btn class="btn-servicios"><a href="/confirmar-entrega"><b>Confirmar entrega</b><btn></a></p>
+      </div>
+      <img class="img-right" src="/img/img_envios.jpg">
     </div>
-
+    </div>
+    <br><br>
   </section>
+
+  <br><br>
+  <section class="soluciones" id="intro">
+    <h1> <strong>» Soluciones de Negocio</strong></h1>
+    <br>
+    QuickHaul te ofrece una variedad de servicios a medida para empresas.
+    Independientemente del tipo de negocio que usted desempeñe, podemos ayudarle a ser más eficiente y exitoso.
+    Por servicios a medida para empresas envíenos un e-mail a quickcarry@contacto.com.uy 
+    detallando sus necesidades.
+  </section>
+  <br><br>
   @include("common/footer")
 </body>
