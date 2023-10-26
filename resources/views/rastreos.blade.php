b58954a574bf384fd8fe210958ee50b2a0aaab8f
Alan Juárez <alanjuarez.n64@gmail.com>
2023-10-26 13:50:24 -0300

    Se agrego una vista para el rastreo de articulos

---
+++ resources/views/rastreos.blade.php
@@ -0,0 +1,16 @@
+@include("common/header")
+<body>
+    <br><br>
+    <section class="Rastreos">
+            <form id="intro">
+                <h1>Rastreos de paqueteria</h1><br>
+                <label for="id">ID del artículo:</label><br>
+                <input type="number" name="id" id="id" class="buscar-id"><br>
+                <button type="submit" value="Buscar" class="enviar">Rastrear</button>
+            </form>
+            <br><br>
+    </section>
+    <br><br>
+</body>
+@include("common/footer")
+</html>
