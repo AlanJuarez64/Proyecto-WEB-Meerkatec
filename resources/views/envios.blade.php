bd1d96aafa9873ccdac4ba024ab0c677cca547e7
Alan Juárez <alanjuarez.n64@gmail.com>
2023-11-02 20:26:37 -0300

    Se agregarón mensajes de confirmación y de avisos a la hora de realizar las confirmaciones de entrega

--- resources/views/envios.blade.php
+++ resources/views/envios.blade.php
@@ -1,13 +1,23 @@
 @include("/common/header")
 <body>
     <section class="confirmar-envios">
-        <form id="intro">
+        <form id="intro" method="post">
+            @csrf
             <h1>Confirmación de envio</h1><br>
             <label for="Correo">Correo Electrónico</label>
-            <input type="email" id="Correo" class="input-mail" required placeholder="email@dominio.com">
+            <input type="email" id="Correo" name='Correo' class="input-mail" required placeholder="email@dominio.com">
             <label for="id">ID del artículo:</label><br>
             <input type="number" name="id" id="id" class="buscar-id"><br>
+            @if(session('success_message'))
+                <p>{{ session('success_message') }}</p>
+            @else
+            <p><i><b>IMPORTANTE<b>: Asegurese de que el correo Electrónico sea el mismo que se ingresó 
+            en la página e-comerce.</i></p>
+            @endif
+            <br>
             <button type="submit" value="enviar" class="enviar">enviar Confirmación</button>
+            <br><br>
+            
         </form>
     </section>
 </body>
