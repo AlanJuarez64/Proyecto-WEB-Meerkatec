6426cc8b7d30ce82f5538d4b5c1a68da68facece
Alan Juárez <alanjuarez.n64@gmail.com>
2023-11-15 20:56:00 -0300

    Se Minimizó la página "acerca de nosotros"

--- resources/views/acercaDeNosotros.blade.php
+++ resources/views/acercaDeNosotros.blade.php
@@ -2,28 +2,23 @@
 <body>
   <br>
   <div id="acerca-de">
-  <h1 class="titulo-principal">Acerca de nuestra empresa.</h1>
-  <h2 class="subtitulo">¿Quiénes somos?</h2>
+  <h1 class="subtitulo">¿Quiénes somos?</h1>
   <section class="mision-vision">
     
 
     <h3>Somos QUICKCARRY</h3>
-      <p id="mision"><em> <strong>Una empresa cuya misión es transformar la forma en que se mueven las mercancías,
-            brindando soluciones de transporte rápido y eficiente que satisfagan las necesidades de nuestros clientes.</strong>
-          <p>Nos esforzamos por ser reconocidos como un referente en el sector,
-            destacándonos por nuestra calidad de servicio y compromiso con la satisfacción del cliente.</p>
-        </em></p>
-
-      <p id="vision"><em> <strong>En QUICKCARRY, nuestra vision es ofrecer soluciones de transporte rápidas,
-            seguras y confiables que impulsen el crecimiento y el éxito de nuestros clientes.</p></strong>
-
-      <p> Nos dedicamos a simplificar y agilizar el proceso de envío, brindando una experiencia sin complicaciones y un servicio excepcional en cada entrega.</p>
-
-      <p>Trabajamos en estrecha colaboración con nuestros clientes, entendiendo sus necesidades únicas y adaptándonos a sus requerimientos específicos.
-      <p>Nos esforzamos por superar las expectativas, proporcionando soluciones eficientes y asequibles que permitan a nuestros clientes enfocarse en su negocio principal sin preocupaciones logísticas.
-        En QuickCarry, nuestro compromiso es ser su socio confiable en el transporte, brindando resultados excepcionales y construyendo relaciones duraderas basadas en la confianza y la satisfacción mutua.</p></strong>
-      </em></p>
+    <div class ="stanza">
+      <h2 id="mision"> <strong>Una empresa cuya misión es transformar la forma en que se mueven las mercancías,
+            brindando soluciones de transporte rápido y eficiente que satisfagan las necesidades de nuestros clientes.</strong></p>
+      </div>
+        <br>
+      <div class ="stanza">
+      <h2 id="vision"> <strong>En QUICKCARRY, nuestra vision es ofrecer soluciones de transporte rápidas,
+            seguras y confiables que impulsen el crecimiento y el éxito de nuestros clientes.</h2></strong>
+        <br>
     </div>
+</div>
+</div>
     <br>
   </section>
   @include("common/footer")
